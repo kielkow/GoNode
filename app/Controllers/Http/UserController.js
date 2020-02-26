e50de8ae9862a3cd42958f33b38a07c691d81add
kielkow <matheuskiel@fiorifer.com.br>
2020-02-25 22:45:40 -0300

    Transactions

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
@@ -7,9 +8,12 @@ class UserController {
     const data = request.only(['username', 'email', 'password'])
     const adresses = request.input('adresses')
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction()
 
-    await user.adresses().createMany(adresses)
+    const user = await User.create(data, trx)
+    await user.adresses().createMany(adresses, trx)
+
+    await trx.commit()
 
     return user
   }
